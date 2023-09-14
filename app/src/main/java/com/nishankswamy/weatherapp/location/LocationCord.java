8f97ea09794eadbe55aea740e6af7bf2a5908152
nishankswamy <nishank.swamy@gmail.com>
2023-09-14 21:44:30 +0200

    build2

---
+++ app/src/main/java/com/nishankswamy/weatherapp/location/LocationCord.java
@@ -0,0 +1,13 @@
+package com.nishankswamy.weatherapp.location;
+
+import com.nishankswamy.weatherapp.BuildConfig;
+
+import java.util.Properties;
+
+public class LocationCord {
+    public static String lat = "";
+    public static String lon = "";
+
+    public final static String API_KEY = BuildConfig.OPENWEATHER_API_KEY;
+}
+
