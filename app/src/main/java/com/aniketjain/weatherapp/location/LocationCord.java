108369db6fe13f1fd4cad3191dd0f24241056af6
nishankswamy <nishank.swamy@gmail.com>
2023-09-16 20:33:28 +0200

    cleanup structre

--- app/src/main/java/com/aniketjain/weatherapp/location/LocationCord.java
+++
@@ -1,9 +0,0 @@
-package com.aniketjain.weatherapp.location;
-
-public class LocationCord {
-    public static String lat = "";
-    public static String lon = "";
-        public final static String API_KEY = "439d4b804bc8187953eb36d2a8c26a02";
-//    public final static String API_KEY = "eeb8b40367eee691683e5a079e2fa695";
-}
-
